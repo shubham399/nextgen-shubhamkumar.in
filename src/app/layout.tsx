a764cc7659263ee06c60da4f37de3f06014f19af
Shubham Kumar <hello@shubhkumar.in>
2026-06-14 13:46:26 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 import MailingListPopup from "@/components/MailingListPopup";
 
 
